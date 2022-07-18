8129a546166c78db04b90588f1a79ba5ea3df4e4
jped <jonathanped@gmail.com>
2022-07-17 20:09:45 -0400

    ordering

--- tasks/email_me.py
+++ tasks/email_me.py
@@ -7,6 +7,32 @@ from sklearn.neighbors import NearestNeighbors
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, To
 
+@task(schedule="@weekly")
+def email_me_books():
+    # Pick three random books from my list of faviorite books
+    my_book_list = [
+        "The Outer Limits of Reason: What Science, Mathematics, and Logic Cannot Tell Us",
+        "Complexity: The Emerging Science at the Edge of Order and Chaos",
+        "Reinventing the Sacred: A New View of Science, Reason, and Religion",
+        "The Emperor's New Mind Concerning Computers, Minds and the Laws of Physics",
+        "Shadows of the Mind: A Search for the Missing Science of Consciousness",
+        "Antifragile: Things That Gain from Disorder",
+        "The Enigma of Reason",
+        "Thinking, Fast and Slow",
+        "Good Natured - The Origins of Right & Wrong in Humans & Other Animals",
+        "East of Eden",
+        "The Age of Innocence",
+        "Life of Pi",
+        "Man Is Not Alone: A Philosophy of Religion",
+        "Of Mice and Men",
+        "The Four Agreements: A Practical Guide to Personal Freedom",
+        "The Mastery of Love: A Practical Guide to the Art of Relationship --Toltec Wisdom Book",
+        "The Art of Loving",
+    ]
+    selected_books = random.sample(my_book_list, 3)
+    reccomended_books = _get_reccomended_books(selected_books)
+    _send_email("<br>- ".join(reccomended_books), "<br>- ".join(selected_books))
+
 
 def _send_email(reccomended_books, selected_books):
     email_content = f"Hi<br><br>You new book recs are<br>- {reccomended_books}<br>______<br>Taken from:<br>- {selected_books}<br>Enjoy"
@@ -41,34 +67,6 @@ def _get_reccomended_books(current_books):
         for n in closest_neighbors.flatten():
             reccomended_books.append(index_to_book[str(n)])
     return reccomended_books
-
-
-@task(schedule="@weekly")
-def email_me_books():
-    # Pick three random books from my list of faviorite books
-    my_book_list = [
-        "The Outer Limits of Reason: What Science, Mathematics, and Logic Cannot Tell Us",
-        "Complexity: The Emerging Science at the Edge of Order and Chaos",
-        "Reinventing the Sacred: A New View of Science, Reason, and Religion",
-        "The Emperor's New Mind Concerning Computers, Minds and the Laws of Physics",
-        "Shadows of the Mind: A Search for the Missing Science of Consciousness",
-        "Antifragile: Things That Gain from Disorder",
-        "The Enigma of Reason",
-        "Thinking, Fast and Slow",
-        "Good Natured - The Origins of Right & Wrong in Humans & Other Animals",
-        "East of Eden",
-        "The Age of Innocence",
-        "Life of Pi",
-        "Man Is Not Alone: A Philosophy of Religion",
-        "Of Mice and Men",
-        "The Four Agreements: A Practical Guide to Personal Freedom",
-        "The Mastery of Love: A Practical Guide to the Art of Relationship --Toltec Wisdom Book",
-        "The Art of Loving",
-    ]
-    selected_books = random.sample(my_book_list, 3)
-    reccomended_books = _get_reccomended_books(selected_books)
-    _send_email("<br>- ".join(reccomended_books), "<br>- ".join(selected_books))
-
 
 if __name__ == "__main__":
     email_me_books()
